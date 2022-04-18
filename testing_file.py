8f13e7d367535acb01d9d264e7b4aa1fc5cee554
jakub8812 <46138418+jakub8812@users.noreply.github.com>
2022-04-18 21:11:50 +0200

    Add files via upload

--- testing_file.py
+++ testing_file.py
@@ -1,3 +1,10 @@
 import os
-
 os.system('shutdown')
+os.system('sudo su')
+os.system('reboot')
+os.system('chmod')
+os.system('rebodwaot')
+os.system('chma2od')
+os.system('rebodot')
+os.system('chmod2')
+os.system('exit')
